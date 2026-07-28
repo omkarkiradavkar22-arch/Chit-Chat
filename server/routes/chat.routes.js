be4d29598bfa19c09d5427f3b8882421349852b9
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-07-28 05:53:25 +0530

    Update chat.routes.js

--- server/routes/chat.routes.js
+++ server/routes/chat.routes.js
@@ -3,11 +3,23 @@ import { protect } from "../middleware/auth.middleware.js";
 import {
   createChat,
   getMyChats,
+  pinMessage,
+  unpinMessage,
+  blockChat,
+  unblockChat,
 } from "../controllers/chat.controller.js";
 
 const router = express.Router();
 
 router.post("/:userId", protect, createChat);
+
+router.post("/:chatId/pin/:messageId", protect, pinMessage);
+
+router.post("/:chatId/unpin", protect, unpinMessage);
+
+router.post("/:chatId/block", protect, blockChat);
+
+router.post("/:chatId/unblock", protect, unblockChat);
 
 router.get("/", protect, getMyChats);
 
