8d9c8a2358b21292b2d387739e4170519a89ef4e
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-07-30 08:39:34 +0530

    Update ChatSidebar.jsx

--- client/src/components/chat/ChatSidebar.jsx
+++ client/src/components/chat/ChatSidebar.jsx
@@ -33,6 +33,8 @@ function ChatSidebar({ chats, loading ,onlineUsers}) {
               chatId === chat._id ? "bg-gray-100" : ""
             }`}
           >
+            <div className="relative">
+
             <img
               src={
                 chat.otherUser?.profilePic ||
@@ -40,11 +42,12 @@ function ChatSidebar({ chats, loading ,onlineUsers}) {
               }
               alt={chat.otherUser?.name}
               className="w-12 h-12 rounded-full object-cover"
-            />
+              />
 
             {onlineUsers.includes(chat.otherUser?._id) && (
-    <span className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 rounded-full border-2 border-white" />
-  )}
+              <span className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 rounded-full border-2 border-white" />
+            )}
+            </div>
 
             <div className="flex-1 overflow-hidden">
 
