893757740e14493aa61285634adc706764ac251c
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-07-31 13:20:17 +0530

    Update Chat.js

--- server/models/Chat.js
+++ server/models/Chat.js
@@ -26,6 +26,50 @@ const chatSchema = new mongoose.Schema(
   default: false,
 },
 
+disappearingMessages: {
+  enabled: {
+    type: Boolean,
+    default: false,
+  },
+  duration: {
+    type: Number,
+    default: null,
+  },
+},
+
+liveLocation: {
+  active: {
+    type: Boolean,
+    default: false,
+  },
+
+  sharedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    default: null,
+  },
+
+  latitude: {
+    type: Number,
+    default: null,
+  },
+
+  longitude: {
+    type: Number,
+    default: null,
+  },
+
+  startedAt: {
+    type: Date,
+    default: null,
+  },
+
+  expiresAt: {
+    type: Date,
+    default: null,
+  },
+},
+
 blockedBy: {
   type: mongoose.Schema.Types.ObjectId,
   ref: "User",
