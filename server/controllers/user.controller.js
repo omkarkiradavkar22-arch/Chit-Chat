3659704f621951fe9ade52070d9ead87db6d6c94
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-07-31 13:09:47 +0530

    Update user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -508,11 +508,16 @@ export const cancelFollowRequest = async (req, res) => {
     });
   }
 };
-const uploadToCloudinary = (buffer, folder) => {
+const uploadToCloudinary = (
+  buffer,
+  folder,
+  resource_type = "auto"
+) => {
   return new Promise((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream(
       {
         folder,
+        resource_type,
       },
       (error, result) => {
         if (error) return reject(error);
