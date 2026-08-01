d19bbf4260f917bba955a28eebf0add2bfeb1867
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-08-01 19:21:01 +0530

    Update sendToken.js

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,12 +1,14 @@
 const sendToken = (user, statusCode, res) => {
   const token = user.generateToken();
 
+  const isProduction = process.env.NODE_ENV === "production";
+
   const options = {
-  httpOnly: true,
-  secure: true,
-  sameSite: "none",
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-};
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  };
 
   res
     .status(statusCode)
