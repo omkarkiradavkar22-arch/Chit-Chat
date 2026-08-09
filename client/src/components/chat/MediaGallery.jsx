eb6960bc901ae476205500a1eb68038bf7323482
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-08-09 21:17:07 +0530

    Update MediaGallery.jsx

--- client/src/components/chat/MediaGallery.jsx
+++ client/src/components/chat/MediaGallery.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../../context/AuthContext";
 import api from "../../services/api";
+import { useTheme } from "../../context/ThemeContext";
 import {
   FaTimes,
   FaFileAlt,
@@ -31,7 +32,8 @@ function formatBytes(bytes) {
 }
 
 function MediaGallery({ chatId, isOpen, onClose }) {
-  const { darkMode } = useAuth();
+  const { theme } = useTheme();
+const darkMode = theme === "dark";
 
   const [activeTab, setActiveTab] = useState("photos");
 
