2f8c0f08e6ae905fbe0bfdc8ec5d86a9ec9e95ad
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-08-10 22:37:03 +0530

    Update Loading.jsx

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,7 +1,9 @@
 function Loading() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">Loading...</h1>
+    <div className="flex items-center justify-center h-screen bg-white dark:bg-gray-950 transition-colors duration-300">
+      <h1 className="text-2xl font-bold text-gray-900 dark:text-white">
+        Loading...
+      </h1>
     </div>
   );
 }
