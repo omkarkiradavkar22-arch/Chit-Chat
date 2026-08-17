d14c2f4fc7c2393343e6201b47672c8a9d1f89c2
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-08-17 20:19:16 +0530

    Add files via upload

---
+++ client/src/pages/Explore.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import Layout from "../components/layouts/Layout";
+import ExploreGrid from "../components/explore/ExploreGrid";
+import api from "../services/api";
+import { toast } from "react-hot-toast";
+
+function Explore() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getExplorePosts = async () => {
+    try {
+      const { data } = await api.get("/posts/explore");
+
+      setPosts(data.posts);
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message ||
+          "Failed to load explore posts"
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getExplorePosts();
+  }, []);
+
+  return (
+    <Layout>
+      <div className="max-w-6xl mx-auto py-5 text-gray-900 dark:text-white">
+
+        <h1 className="text-3xl font-bold mb-6 text-gray-900 dark:text-white">
+          Explore
+        </h1>
+
+        {loading ? (
+          <p className="text-center text-gray-600 dark:text-gray-300">
+  Loading...
+</p>
+        ) : (
+          <ExploreGrid posts={posts} />
+        )}
+
+      </div>
+    </Layout>
+  );
+}
+
+export default Explore;
