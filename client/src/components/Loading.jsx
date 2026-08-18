bca67eb45a0fd6bcbe202dee35643eaef951726e
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-08-18 19:19:48 +0530

    Delete client directory

--- client/src/components/Loading.jsx
+++
@@ -1,9 +0,0 @@
-function Loading() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">Loading...</h1>
-    </div>
-  );
-}
-
-export default Loading;
