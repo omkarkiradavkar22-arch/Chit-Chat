d62be2b78e08b29fc5a0c6dd7968180f73cee9ef
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-08-19 22:26:25 +0530

    Update MobileNavbar.jsx

--- client/src/components/layouts/MobileNavbar.jsx
+++ client/src/components/layouts/MobileNavbar.jsx
@@ -1,5 +1,4 @@
-import { FaHome, FaSearch, FaCompass, FaBell, FaComments,
-} from "react-icons/fa";
+import { FaHome, FaSearch, FaCompass, FaBell, FaComments, } from "react-icons/fa";
 import { useAuth } from "../../context/AuthContext";
 import { Link, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
