9cbd875ebf23e7e7a266f4426c3cedc7e3be03db
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-08-24 22:54:33 +0530

    Update ChatHeader.jsx

--- client/src/components/chat/ChatHeader.jsx
+++ client/src/components/chat/ChatHeader.jsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { FaPhone, FaVideo, FaEllipsisV,FaArrowLeft, FaSearch,
-  FaImages,
+import { FaPhone, FaVideo, FaEllipsisV,FaArrowLeft, FaSearch, FaImages,
   FaTimes,
   FaCalendar,
 FaClock,
