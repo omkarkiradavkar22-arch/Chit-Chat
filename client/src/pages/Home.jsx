7a54c55c1d11a5cc72c80f5e3f2d1b39a3e537c9
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-08-26 21:26:13 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -39,8 +39,8 @@ function Home() {
 </p>
         ) : posts.length === 0 ? (
           <p className="text-center text-gray-500 dark:text-gray-400">
-  No posts yet.
-</p>
+            No posts yet.
+          </p>
         ) : (
           posts.map((post) => (
             <PostCard
