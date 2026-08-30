1164aa9d890dedf0b987b67003c46eff470b035e
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-08-30 09:57:22 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import AppRoutes from "./routes/AppRoutes";
 import CallOverlay from "./components/call/CallOverlay";
+import NotificationPermissionBanner from "./components/NotificationPermissionBanner";
 
 function App() {
   return (
     <>
       <AppRoutes />
       <CallOverlay />
+      <NotificationPermissionBanner />
     </>
   );
 }
