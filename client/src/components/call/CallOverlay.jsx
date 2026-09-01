986f392e4d0625c0178a47df4d402274daf4b876
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-09-01 19:50:15 +0530

    Update CallOverlay.jsx

--- client/src/components/call/CallOverlay.jsx
+++ client/src/components/call/CallOverlay.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useRef } from "react";
 import { useCall } from "../../context/CallContext";
 import { useTheme } from "../../context/ThemeContext";
-import { FaPhone, FaPhoneSlash, FaMicrophone, FaMicrophoneSlash, FaVideo, FaVideoSlash,
+import {
+  FaPhone,
+  FaPhoneSlash,
+  FaMicrophone,
+  FaMicrophoneSlash,
+  FaVideo,
+  FaVideoSlash,
 } from "react-icons/fa";
 
 function formatDuration(seconds) {
