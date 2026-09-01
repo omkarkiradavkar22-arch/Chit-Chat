fc036de529a4ad1c417c695f7c0457966f6f4b05
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-09-01 19:51:29 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,11 @@
 import AppRoutes from "./routes/AppRoutes";
 import CallOverlay from "./components/call/CallOverlay";
-import IncomingCallPopup from "./components/call/IncomingCallPopup";
 import NotificationPermissionBanner from "./components/NotificationPermissionBanner";
 
 function App() {
   return (
     <>
       <AppRoutes />
-
-      {/* Incoming call popup */}
-      <IncomingCallPopup />
-
       {/* Connected / outgoing call UI */}
       <CallOverlay />
 
