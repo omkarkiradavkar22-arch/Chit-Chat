4840944816882d1d61f2f76901731591cb67a526
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-09-15 10:13:32 +0530

    Update auth.routes.js

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,11 +1,30 @@
 import express from "express";
-import { registerUser, loginUser, logoutUser,getMe } from "../controllers/auth.controller.js";
+
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getMe,
+  forgotPassword,
+  resetPassword,
+} from "../controllers/auth.controller.js";
+
 import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.post("/login",loginUser);
-router.post("/logout",logoutUser);
-router.get("/me",protect,getMe);
+
+router.post("/login", loginUser);
+
+router.post("/logout", logoutUser);
+
+router.get("/me", protect, getMe);
+
+// Forgot Password
+router.post("/forgot-password", forgotPassword);
+
+// Reset Password
+router.post("/reset-password/:token", resetPassword);
+
 export default router;
