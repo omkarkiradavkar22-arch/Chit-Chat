55a279957c5d2b3e30c91a0db02e983f46cfff79
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-09-18 23:11:28 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import AppRoutes from "./routes/AppRoutes";
 import CallOverlay from "./components/call/CallOverlay";
 import NotificationPermissionBanner from "./components/NotificationPermissionBanner";
+import OfflineBanner from "./components/OfflineBanner";
 
 function App() {
   return (
     <>
+     <OfflineBanner />
+     
       <AppRoutes />
       {/* Connected / outgoing call UI */}
       <CallOverlay />
